f7bc84fecfedbb6180498a20c34a2907dc1c8631
Vidya1026 <146586310+Vidya1026@users.noreply.github.com>
2024-04-11 01:22:04 +0530

    Update 4. Man.js

--- 4. Man.js
+++ 4. Man.js
@@ -1,3 +1,9 @@
+// There are 100 man making a circle each man is wearing a T-shit with a number 1 to 100
+in series. Person with Number 1 on his/her T-Shirt got a gun now 1 kill 2 and give that
+gun to 3 and then 3 kill 4 and give that gun to 5.. then so on 99 killed 100 and give that
+gun again to 1 WAP to find which man is left with a gun on his hand at the end ??
+
+
 function findSurvivor(numberOfPeople) {
   // Handle invalid input (less than 2 people)
   if (numberOfPeople < 2) {
